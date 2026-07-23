846e93fcf0f15b109fb4b60acc1ff3bf93a00f8f
Niteesh gandham <niteesh38@gmail.com>
2026-07-23 18:08:17 +0530

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -2,6 +2,7 @@ locals {
     ami_id = data.aws_ami.joindevops.id
     database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_id.value)[0]   
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
     tags = {
         Name = "${var.project}-${var.environment}"
         environment = var.environment
