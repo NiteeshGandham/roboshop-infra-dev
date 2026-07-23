6e30b555156b8da19cea93f85a1dc3ddb6aeddfc
Niteesh gandham <niteesh38@gmail.com>
2026-07-23 17:34:57 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -34,3 +34,43 @@ resource "terraform_data" "bootstrap" {
     ]
   }
 }
+
+
+#redis
+
+resource "aws_instance" "mongodb" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.redis_sg_id]
+
+  tags =merge(
+    local.tags,
+     {
+        Name = "${var.project}-${var.environment}-mongodb"
+     }
+     
+  )
+}
+
+resource "terraform_data" "bootstrap" {
+   triggers_replace  = [aws_instance.mongodb.id]
+
+    connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.mongodb.private_ip
+  }
+
+   provisioner "file" {
+    source      = "bootstrap.sh" #local file path
+    destination = "/tmp/bootstrap.sh" #destination path in remote
+  }
+   provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mongodb" 
+    ]
+  }
+}
--- 40-databases/variables.tf
+++ 40-databases/variables.tf
@@ -7,3 +7,12 @@ variable "environment" {
     type = string
     default = "dev"
 }
+
+
+variable "zone_id" {
+    default = "Z0064105SFSYFTWS9GZ0"
+}
+
+variable "domain_name" {
+    default = "ngandham.online"
+}
