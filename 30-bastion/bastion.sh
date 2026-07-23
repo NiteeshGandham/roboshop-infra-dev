55de009ae436c4e330edb577f00d4fa430e71e15
Niteesh gandham <niteesh38@gmail.com>
2026-07-23 22:07:02 +0530

    terraform

--- 30-bastion/bastion.sh
+++ 30-bastion/bastion.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-#e are created 50Gb root disk. but 20gb is patitoned
-#remaining 30Gb we need to extend using below cmnds
-growpart /dev/nvme0 4
-lvextend -r -L+30G /dev/mapper/RootVG-homeVol
-xfx_growfs /home
+# Expand the root partition to use the full EBS volume
+sudo growpart /dev/nvme0n1 4
 
-#install terraform
+# Resize the LVM physical volume
+sudo pvresize /dev/nvme0n1p4
+
+# Extend the /home logical volume by 30 GB and resize the filesystem
+sudo lvextend -r -L +30G /dev/mapper/RootVG-homeVol
+
+# Install Terraform
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
-sudo yum -y install terraform
-
+sudo yum install -y terraform
