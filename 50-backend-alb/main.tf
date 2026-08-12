2e541137f4040e20bcc85701b0a949d8563aebbc
Niteesh gandham <niteesh38@gmail.com>
2026-08-12 18:10:31 +0530

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "backend-alb" {
   internal           = true
   load_balancer_type = "application"
   security_groups    = [local.backend_alb_sg_id]
-  subnets            = [local.private_subnet_ids]
+  subnets            = local.private_subnet_ids
 
   enable_deletion_protection = false # we cant delete using terraform if we give true. so for practice
 
