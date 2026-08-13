7ff2d461cdb7b2134c9e1d9c4ac6480434660a9f
Niteesh gandham <niteesh38@gmail.com>
2026-08-13 19:39:54 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -160,6 +160,7 @@ resource "aws_autoscaling_policy" "catalogue" {
   name                   = "${var.project}-${var.environment}-catalogue"
   policy_type        = "TargetTrackingScaling"
   autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  estimated_instance_warmup = 120
 
   target_tracking_configuration {
     predefined_metric_specification {
