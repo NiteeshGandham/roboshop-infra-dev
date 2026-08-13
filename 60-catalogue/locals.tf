15f12d28e2a3d6918e3076ce6864197514c28a3e
Niteesh gandham <niteesh38@gmail.com>
2026-08-13 13:56:02 +0530

    terraform

---
+++ 60-catalogue/locals.tf
@@ -0,0 +1,10 @@
+locals {
+    ami_id = data.aws_ami.joindevops.id
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_id.value)[0]   
+    catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    tags = {
+        Name = "${var.project}-${var.environment}"
+        environment = var.environment
+        terraform = "true"
+    }
+}
---
+++ 60-catalogue/main.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "catalogue" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.private_subnet_id
+  vpc_security_group_ids = [local.catalogue_sg_id]
+
+  tags =merge(
+    local.tags,
+     {
+        Name = "${var.project}-${var.environment}-catalogue"
+     }
+     
+  )
+}
+
+resource "terraform_data" "bootstrap" {
+   triggers_replace  = [aws_instance.catalogue.id]
+
+    connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.catalogue.private_ip
+  }
+
+   provisioner "file" {
+    source      = "bootstrap.sh" #local file path
+    destination = "/tmp/bootstrap.sh" #destination path in remote
+  }
+   provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh catalogue" 
+    ]
+  }
+}
