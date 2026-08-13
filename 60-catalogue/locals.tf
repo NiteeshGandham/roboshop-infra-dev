bf50531175aafbb01f92807e752686f853002a20
Niteesh gandham <niteesh38@gmail.com>
2026-08-13 18:25:08 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -2,7 +2,8 @@ locals {
     ami_id = data.aws_ami.joindevops.id
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_id.value)[0]   
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
-    backend_alb_listner_arn = data.backend_alb_listner_arn.value
+    backend_alb_listner_arn = data.aws_ssm_parameter.backend_alb_listner_arn.value
+     vpc_id = data.aws_ssm_parameter.vpc_id.value
     tags = {
         Name = "${var.project}-${var.environment}"
         environment = var.environment
