01a439b6cad3b0ab06538fd61cfb46449d2f9742
Niteesh gandham <niteesh38@gmail.com>
2026-08-13 14:37:55 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -44,5 +44,5 @@ resource "aws_instance_state" "catalogue" {
 resource "aws_ami_from_instance" "catalogue" {
   name = "${var.project}-${var.environment}-catalogue"
   source_instance_id = aws_instance.catalogue.id
-  depends_on = aws_instance_state.catalogue
+  depends_on = [aws_instance_state.catalogue]
 }
