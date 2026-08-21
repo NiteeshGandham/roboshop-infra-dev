b984a036763fe1a7848c449d0f798699328fd8e5
Niteesh gandham <niteesh38@gmail.com>
2026-08-21 16:34:08 +0530

    ansible roles

--- 60-catalogue/bootstrap.sh
+++ 60-catalogue/bootstrap.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+set -e
+
 component=$1
+
 sudo dnf install ansible -y
 
 cd /home/ec2-user
-git clone https://github.com/NiteeshGandham/ansible-roboshop-roles-tf.git
+
+if [ ! -d "ansible-roboshop-roles-tf" ]; then
+    git clone https://github.com/NiteeshGandham/ansible-roboshop-roles-tf.git
+fi
 
 cd ansible-roboshop-roles-tf
+
 git pull
 
-ansible-playbook -e "component=${component}" roboshop.yaml
+ansible-playbook -e "component=${component}" -i inventory.ini roboshop.yaml
