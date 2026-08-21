9f355a7ae9b3177a2c2170d3724c7c513f3f1d8e
Niteesh gandham <niteesh38@gmail.com>
2026-08-21 14:00:53 +0530

    ansible roles

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
-module "components" {
+module "component" {
     for_each = var.components
     source = "git::https://github.com/NiteeshGandham/terraform-roboshop-component.git?ref=main"
-    components = each.key
+    component = each.key
     rule_priority = each.value.rule_priority
 }
