30346274904227247fff74847eb943593211f73e
Niteesh gandham <niteesh38@gmail.com>
2026-08-21 15:43:51 +0530

    ansible roles

--- 20-sg_rules/main.tf
+++ 20-sg_rules/main.tf
@@ -334,3 +334,13 @@ resource "aws_security_group_rule" "redis_user" {
     source_security_group_id  = local.frontend_alb_sg_id
     security_group_id = local.frontend_sg_id
   }
+
+   resource "aws_security_group_rule" "frontend_bastion" {
+    type              = "ingress"
+    from_port         = 80
+    to_port           = 80
+    protocol          = "tcp"
+    #where traffic coming from
+    source_security_group_id  = local.Bastion_sg_id
+    security_group_id = local.frontend_alb_sg_id
+  }
