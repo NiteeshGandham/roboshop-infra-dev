387a6957c302e3a8b0cc10f5ed90c39ad8da7211
Niteesh gandham <niteesh38@gmail.com>
2026-08-21 15:26:03 +0530

    ansible roles

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -4,7 +4,7 @@ variable "components" {
         catalogue = {
             rule_priority = 10
         }
-        user = {
+/*         user = {
             rule_priority = 20
         }
         cart = {
@@ -15,7 +15,7 @@ variable "components" {
         }
         payment = {
             rule_priority = 50
-        }
+        } */
 
          #frontend component components are attached to frontend alb, there is only one component there
 
